029ee530bd76d4b43dcc1f4d722b4548dd084e2c
Shooter <b2234913@gmail.com>
2020-01-19 00:01:39 +0800

    init commit

---
+++ auto_lend.py
@@ -0,0 +1,80 @@
+from bfxapi import Client, Order
+import datetime
+import time
+import math
+import asyncio
+import os
+import sys
+import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG, 
+                    format='%(asctime)s - %(levelname)s : %(message)s', 
+                    filename='auto_lend.log')
+
+
+with open('config.json' , 'r') as reader:
+    key = json.loads(reader.read())
+
+bfx = Client(key['API_KEY'], key['API_SECRET'], logLevel='DEBUG')
+now = int(round(time.time() * 1000))
+then = now - (1000 * 60 * 60 * 24 * 10) # 10 days ago
+
+async def wallet_available_balance():
+    balance = await bfx.rest.get_available_balance('fUSD', 1, 800, 'FUNDING')
+    return round(balance[0]*(-1),3)-0.001
+
+async def get_books():
+    books = await bfx.rest.get_public_books('fUSD','R0',25)
+    max_funding_rate_want_borrow = books[0][2]
+    tmp = 0
+    money = 0
+    for book in books:
+        if book[3] > 0:
+            tmp += book[2] * book[3]
+            money += book[3] 
+    funding_rate = round(tmp / money, 8)
+    if max_funding_rate_want_borrow > funding_rate:
+        return max_funding_rate_want_borrow
+    else:
+        return funding_rate
+
+async def wallet_funding_balance():
+    amounts = await bfx.rest.get_wallets()
+    for amount in amounts:
+        if amount.__dict__['key'] == 'funding_USD':            
+            balance = math.ceil(amount.__dict__['unsettled_interest']*100)/100
+    return balance
+
+async def create_funding_order(funding_rate, balance):
+    try:
+        resp = await bfx.rest.submit_funding_offer("fUSD", balance, funding_rate, 2)
+        logging.info(resp)
+    except Exception as e:
+        logging.error(e)
+
+async def check_offer():
+    offers = await bfx.rest.get_funding_offers('fUSD')
+    for offer in offers:
+        offer_data = offer.__dict__
+        now = math.ceil(time.time()*1000)
+        if now - offer_data["mts_create"] > 1000*60*2:
+            try:
+                resp = await bfx.rest.submit_cancel_funding_offer(offer_data['id'])
+                logging.info(resp)
+            except Exception as e:
+                logging.error(e)
+
+async def run():
+    await check_offer()
+    balance = await wallet_available_balance()
+    funding_rate = await get_books()
+    funding_rate_year = round(math.pow(1 + funding_rate, 365) - 1, 4)
+    logging.info("Balance: %.2f, Rate: %.8f, Year: %.4f", balance, funding_rate, funding_rate_year)
+    if balance > 50:
+        await create_funding_order(funding_rate, balance)
+
+while 1 :
+    t = asyncio.ensure_future(run())
+    asyncio.get_event_loop().run_until_complete(t)
+    time.sleep(10)
